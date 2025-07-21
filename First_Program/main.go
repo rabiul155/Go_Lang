54e1e04b47ca8aadc2a243d27bdc810867543c52
rabiul155 <siyamict@gmail.com>
2025-07-21 22:18:08 +0600

    first commit

---
+++ First_Program/main.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+func helloWorld (){
+	fmt.Println("Hello, world!")
+
+	//declare variable 
+	var a int = 10
+	fmt.Println(a);
+
+	//Variable declare sort-hand
+	x := 100
+	fmt.Println(x)
+
+	//variable redeclare 
+	m :=true
+	m = false
+	fmt.Println(m)
+}
+
+func ifelse (){
+	a := 10
+
+	if a > 10 {
+		fmt.Println("Number is greater than 10")
+	}else if a < 10 {
+		fmt.Println("Number is less than 10")
+	} else{
+		fmt.Println("Number is 10")
+	}
+
+}
+
+func switchCase (){
+	a :=3
+
+	switch a {
+	case 1 :{
+		fmt.Println("a is one")
+		
+	}
+	case 2, 3 : {
+		fmt.Println("a is either two or thee")
+	}
+	default : {
+		fmt.Println(" a is nothing")
+	}
+	}
+}
+
+func addFunc (num1 int, num2 int){
+
+	sum := num1 + num2
+
+	fmt.Println(sum)
+}
+
+func main(){
+	helloWorld()
+	ifelse()
+	switchCase()
+	a:=10
+	b:=20
+	addFunc(a,b)
+
+}
